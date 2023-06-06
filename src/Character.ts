d3d5012aeaf7b812a8f20e9c2ed63ada5d9911ba
Jainefranciellem <jainefraciellen@gmail.com>
2023-06-06 19:05:44 -0300

    created class Monster

--- src/Character.ts
+++ src/Character.ts
@@ -1,6 +1,7 @@
 import { Mage } from './Archetypes';
 import Archetype from './Archetypes/Archetype';
 import Energy from './Energy';
+import { SimpleFighter } from './Fighter';
 import Fighter from './Fighter/Fighter';
 import Race, { Elf } from './Races';
 import getRandomInt from './utils';
@@ -88,7 +89,7 @@ export default class Character implements Fighter {
     return this._lifePoints;
   }
 
-  attack(enemy: Fighter): void {
+  attack(enemy: Fighter | SimpleFighter): void {
     enemy.receiveDamage(this.strength);
   }
 
