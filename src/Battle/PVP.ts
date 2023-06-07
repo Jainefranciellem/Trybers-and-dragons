ebe7955aec0d1dd7154fd4fe37497aef9d4dea77
Jainefranciellem <jainefraciellen@gmail.com>
2023-06-07 15:15:13 -0300

    fix lint

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -15,7 +15,8 @@ export default class PVP extends Battle {
     do {
       this._player1.attack(this._player2);
       this._player2.attack(this._player1);
-    } while (this._player2.lifePoints !== -1 && this._player1.lifePoints !== -1)
+    }
+    while (this._player2.lifePoints !== -1 && this._player1.lifePoints !== -1);
     return super.fight();
   }
 }
